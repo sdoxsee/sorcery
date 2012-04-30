cec5ea90533decfc1d0d6e7aaf0a5d599ee3b6d5
Stephen Doxsee <stephen@doxsee.org>
2012-04-30 17:45:18 -0400

    initial commit with sorcery, rspec, guard and basic user scaffold

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  authenticates_with_sorcery!
+
+  attr_accessible :email, :password, :password_confirmation
+
+  validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
+  validates_confirmation_of :password, :message => "should match confirmation", :if => :password
+  validates_presence_of :password, :on => :create
+  validates_presence_of :email
+  validates_uniqueness_of :email
+end
